aca3436a20a9e5943ed719137bcb65f0b137afdd
wholzapfel <werner.holzapfel@trivento.nl>
2020-03-03 12:51:24 +0100

    styling aangepast

--- src/app/pages/predictions_tab/matches/matches.page.ts
+++ src/app/pages/predictions_tab/matches/matches.page.ts
@@ -25,14 +25,18 @@ export class MatchesPage implements OnInit, OnDestroy {
         this.setMatches();
     }
 
+    scrollSegments(index: number) {
+        const segment = document.querySelector('ion-segment');
+        const active = segment.querySelectorAll('ion-segment-button')[index];
+        active.scrollIntoView({behavior: 'smooth', inline: 'center'});
+
+    }
     ngOnInit() {
         this.matchService.getMatchPredictions().subscribe(
             matchPredictions => {
                 this.allMatchPredictions = matchPredictions;
                 this.setMatches();
             });
-
-
     }
 
     setMatches() {
@@ -69,6 +73,16 @@ export class MatchesPage implements OnInit, OnDestroy {
     // navigate(item) {
     //   this.router.navigate(['/list', JSON.stringify(item)]);
     // }
+
+    // async slideChanged() {
+    //     this.activeIndex= await this.slider.getActiveIndex();
+    //     document.getElementById("segment-" + activeIndex).scrollIntoView({
+    //         behavior: 'smooth',
+    //         block: 'center',
+    //         inline: 'center'
+    //     });
+    // }
+
     ngOnDestroy(): void {
         this.unsubscribe.next();
         this.unsubscribe.unsubscribe();
